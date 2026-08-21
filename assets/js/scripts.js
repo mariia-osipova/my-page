72ab0acaa4f329ec6b58d72d038f4c7f3b1ec72a
Mariia Osipova <maryosipovaa@gmail.com>
2026-08-21 20:18:15 -0300

    moving line

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,28 +1,7 @@
-// Sticky header position on page scrolling up
 const header = document.querySelector('.js-header');
-const stickyClass = 'sticky';
-let lastScrollTop = 0;
-let isWaiting = false;
-
-window.addEventListener('scroll', () => {
-    if (!isWaiting) {
-        window.requestAnimationFrame(() => {
-            let currentScroll = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
-
-            if (currentScroll > lastScrollTop) {
-                header.classList.remove(stickyClass);
-            } else if (currentScroll < lastScrollTop && currentScroll > 0) {
-                header.classList.add(stickyClass);
-            } else if (currentScroll <= 0) {
-                header.classList.remove(stickyClass);
-            }
-
-            lastScrollTop = currentScroll;
-            isWaiting = false;
-        });
-        isWaiting = true;
+if (header) {
+    header.classList.add('sticky');
     }
-}, false);
 
 // Dropdown menu
 (function (menuConfig) {
